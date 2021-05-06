efd4428b61f84314cd2104ed79b44c7a6c6baaa0
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2021-05-06 13:09:53 +0430

    feat: window starts at maximum size

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const puppeteer = require('puppeteer');
 const app = require('./src/app');
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false
-  });
-  app(browser)
-})();
+app();
--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
+const puppeteer = require('puppeteer');
 const TrackingPage = require('./tracker/TrackingPage')
 
-module.exports = async function app(browser) {
-  const page = new TrackingPage(browser)
+module.exports = async function app() {
+  // Launching Chromium
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      '--auto-open-devtools-for-tabs',
+      '--window-size=1920,1080',
+    ],
+    defaultViewport: null
+  });
+
+  // Creating a new tracking page
+  new TrackingPage(browser)
 }
