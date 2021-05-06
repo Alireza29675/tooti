73531ff86eed6fb3700e65b4cd6416c96cd0f1f5
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2021-05-06 12:24:45 +0430

    refactor: moved tracking modules to tracker folder

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const TrackingPage = require('./TrackingPage')
+const TrackingPage = require('./tracker/TrackingPage')
 
 module.exports = async function app(browser) {
   const page = new TrackingPage(browser)
