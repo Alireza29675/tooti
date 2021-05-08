0ca3d72daca1b5b34db40000ddc8a1d159ec4f1f
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2021-05-08 18:35:31 +0430

    feat: update app function

--- src/app.js
+++ src/app.js
@@ -1,26 +1,6 @@
-const puppeteer = require('puppeteer');
-
-const { createTrackingPage } = require('./tracker')
-const { automate } = require('./automator')
+const { createBrowser, automate } = require('./core')
 
 module.exports = async function app() {
-  // Launching Chromium
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: [
-      '--auto-open-devtools-for-tabs',
-      '--window-size=1920,1080',
-    ],
-    defaultViewport: null
-  });
-
-  // createTrackingPage({
-  //   id: 'test',
-  //   browser
-  // })
-
-  automate({
-    id: 'test',
-    browser
-  })
+  const browser = await createBrowser()
+  automate({ id: 'test', browser })
 }
