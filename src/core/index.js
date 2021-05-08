66983bfcacd1f288eae09dbd076b46b5a1b1bfd2
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2021-05-08 18:35:08 +0430

    feat: core index exposes functions of browser

---
+++ src/core/index.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+const { createTrackingPage } = require('./core/tracker')
+const { automate } = require('./automator')
+
+module.exports = {
+  async createBrowser() {
+    return await puppeteer.launch({
+      headless: false,
+      args: [
+        '--auto-open-devtools-for-tabs',
+        '--window-size=1920,1080',
+      ],
+      defaultViewport: null
+    });
+  },
+  createTrackingPage,
+  automate
+}
