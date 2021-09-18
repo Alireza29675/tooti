bb9507bccea8e3f32ed0c0e05c65342d7a35cb7c
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2021-09-18 19:28:30 +0430

    feat: there is a opening page

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -1,26 +1,35 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
+const locateChrome = require('locate-chrome');
 
 const { createTrackingPage } = require('./tracker')
 const { automate } = require('./automator')
 
 module.exports = async function app() {
+  // Finding where the chrome is
+  const chromePath = await locateChrome();
+
   // Launching Chromium
   const browser = await puppeteer.launch({
     headless: false,
+    executablePath: chromePath,
     args: [
-      '--auto-open-devtools-for-tabs',
-      '--window-size=1920,1080',
+      // '--auto-open-devtools-for-tabs',
+      '--window-size=400,800',
     ],
     defaultViewport: null
   });
+
+  const openingPage = (await browser.pages())[0];
+
+  openingPage.goto('https://www.google.com/');
 
   // createTrackingPage({
   //   id: 'test',
   //   browser
   // })
 
-  automate({
-    id: 'test',
-    browser
-  })
+  // automate({
+  //   id: 'test',
+  //   browser
+  // })
 }
